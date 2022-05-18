d537773382a286db93e5ca8af16d36d40560343a
8ball <se.pfeifer@duocuc.cl>
2022-05-18 12:52:55 -0400

    apis

--- js/main.js
+++ js/main.js
@@ -244,10 +244,10 @@ $(function() {
         });
     });
 
-    /*header2 */
+    /*header */
 $(function() {
     $.get("header copy.html", function(data) {
-        document.getElementById("header2").outerHTML = data;
+        document.getElementById("header").outerHTML = data;
     });
 });
 
@@ -259,4 +259,21 @@ $(function() {
     });
 });
 
+$(document).ready(function() {
+    $('#btnMostrarRopa').click(function(){
+        $.get("http://fakestoreapi.com/products",
+            function(data) {
+                debugger
+                $.each(data, 
+                    function(i, item) {
+                        var td1 = `<td>${item.title}</td>`;
+                        var td3 = `<td>${item.price}</td>`;
+                        var td4 = `<td>${item.description}</td>`;
+                        var td2 = `<td><img class="ropa" src="${item.image}"></td>`;
+                        var tr = `<tr>${td1}${td3}${td4}${td2}</tr>`;
+                        $('#tblCategorias').append(tr);
+                    });
+            });
+    });
+});
 
