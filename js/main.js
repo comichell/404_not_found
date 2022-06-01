84b4ae70eb78353044e034416ea395d2ff5badfd
8ball <se.pfeifer@duocuc.cl>
2022-06-01 10:57:35 -0400

    modificacion de la apis, agregacion de monto total de carrito

--- js/main.js
+++ js/main.js
@@ -252,21 +252,23 @@ $(function() {
 });
 
 
+
+
 $(document).ready(function() {
-    $('#btnMostrarRopa').click(function(){
-        $.get("http://fakestoreapi.com/products",
-            function(data) {
-                debugger
-                $.each(data, 
-                    function(i, item) {
-                        var td1 = `<td>${item.title}</td>`;
-                        var td3 = `<td>${item.price}</td>`;
-                        var td4 = `<td>${item.description}</td>`;
-                        var td2 = `<td><img class="ropa" src="${item.image}"></td>`;
-                        var tr = `<tr>${td1}${td3}${td4}${td2}</tr>`;
-                        $('#tblCategorias').append(tr);
-                    });
-            });
-    });
+  
+    $.get("http://fakestoreapi.com/products",
+        function(data) {
+            $.each(data, 
+                function(i, item) {
+                    var td1 = `<td>${item.title}</td>`;
+                    var td3 = `<td>${item.price}</td>`;
+                    var td4 = `<td>${item.description}</td>`;
+                    var td2 = `<td><img class="ropa" src="${item.image}"></td>`;
+                    var tr = `<tr>${td1}${td3}${td4}${td2}</tr>`;
+                    $('#tblCategorias').append(tr);
+                });
+        });
+
 });
+
 
